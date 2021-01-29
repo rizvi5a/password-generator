3b295933d18e6d1b4fc566f823de019e93ff2b1c
Syed Rizvi <rizvi5a@yahoo.com>
2021-01-28 21:48:25 -0600

    added prompy

--- asset/script.js
+++ asset/script.js
@@ -1,1 +1,6 @@
 console.log("javascript loaded")
+ var savebtn=document.getElementById("password")
+ savebtn.addEventListener("click", function() {
+     var length=prompt("enter length of password")
+ }
+ )
