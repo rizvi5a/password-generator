8171416ebde0350fc00ec310d5ad23031cc38fc8
Syed Rizvi <rizvi5a@yahoo.com>
2021-02-04 16:14:19 -0600

    Length validation incorporated

--- asset/script.js
+++ asset/script.js
@@ -1,6 +1,21 @@
 console.log("javascript loaded")
  var savebtn=document.getElementById("password")
  savebtn.addEventListener("click", function() {
-     var length=prompt("enter length of password")
+   getPasswordRequirements()
  }
  )
+
+
+ function getPasswordRequirements(){
+    var length= parseInt(prompt("enter length of password"))
+    if(length <9 || length > 127){
+        alert("Invalid Password length!")
+    }
+    else {
+        
+        var yesUpper = confirm("Do you like to have uppercase letters in your password ?")
+        var yesLower = confirm("Do you like to have lowercase letters in your password ?")
+        var yesSymbols = confirm("Do you like to have symbols letters in your password ?")
+        var yesNumbers = confirm("Do you like to have numbers letters in your password ?")
+    }
+ }
