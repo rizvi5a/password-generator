f16d87eb4f0db5fac0accabca97c724afb594856
Syed Rizvi <rizvi5a@yahoo.com>
2021-02-04 16:28:13 -0600

    option validation is done

--- asset/script.js
+++ asset/script.js
@@ -1,4 +1,8 @@
 console.log("javascript loaded")
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var numberString = "1234567890"
+var symbolString = "!@#$%^&*()_-"
  var savebtn=document.getElementById("password")
  savebtn.addEventListener("click", function() {
    getPasswordRequirements()
@@ -12,10 +16,28 @@ console.log("javascript loaded")
         alert("Invalid Password length!")
     }
     else {
-        
+        do{
+            //Confirm ok- true cancel-false
+            alert("Choose at least one option")
         var yesUpper = confirm("Do you like to have uppercase letters in your password ?")
         var yesLower = confirm("Do you like to have lowercase letters in your password ?")
         var yesSymbols = confirm("Do you like to have symbols letters in your password ?")
         var yesNumbers = confirm("Do you like to have numbers letters in your password ?")
+        } while(!yesLower && !yesUpper && !yesSymbols && !yesNumbers)
+
+        var userChoiceList = []
+        if(yesLower){
+            userChoiceList += lowerCase
+        }
+        if(yesUpper){
+            userChoiceList += upperCase
+        }
+        if(yesNumbers){
+            userChoiceList += numberString
+        }
+        if(yesSymbols){
+            userChoiceList += symbolString
+        }
+        console.log(userChoiceListgitg)
     }
  }
